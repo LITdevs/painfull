48e85b94d246e3427c98fc0d5d3e3b300d42eef6
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-03 17:24:21 +0200

    implement eventhandlers? removed vukky from owner list to not spam his dms while working on bot

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const pjson = require("../package.json")
 require("dotenv").config();
 let spinner;
 let bootDMs = [];
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 client.commands = new Collection();
 console.clear();
 console.log(chalk.greenBright("Painfull"));
@@ -56,6 +56,7 @@ function validateModule(moduleName) {
 	const packageJsonExists = fs.existsSync(moduleFolder + "/package.json")
 	if(!packageJsonExists) bootDMs.push("package.json missing in module:" + moduleName);
 	if(!packageJsonExists) return false; //yes, good
+	
 	return true //figured it out. i never returned true if it succeeded lmfao
 }
 
@@ -103,8 +104,11 @@ function validateManifest(moduleFolder) {
 	if(painVersions[1] < minVersions[1] && painVersions[0] <= minVersions[0]) return false
 	if(painVersions[2] < minVersions[2] && painVersions[1] <= minVersions[1] && painVersions[0] <= minVersions[0]) bootDMs.push("7 module does not support this version of pain:" + moduleName)
 	if(painVersions[2] < minVersions[2] && painVersions[1] <= minVersions[1] && painVersions[0] <= minVersions[0]) return false
-	
-
+	let validEvents = ["apiRequest", "apiResponse", "applicationCommandCreate", "applicationCommandDelete", "applicationCommandUpdate", "channelCreate", "channelDelete", "channelPinsUpdate", "channelUpdate", "debug", "emojiCreate", "emojiDelete", "emojiUpdate", "error", "guildBanAdd", "guildBanRemove", "guildCreate", "guildDelete", "guildIntegrationsUpdate", "guildMemberAdd", "guildMemberAvailable", "guildMemberRemove", "guildMembersChunk", "guildMemberUpdate", "guildScheduledEventCreate", "guildScheduledEventDelete", "guildScheduledEventUpdate", "guildScheduledEventUserAdd", "guildScheduledEventUserRemove", "guildUnavailable", "guildUpdate", "interaction", "interactionCreate", "invalidated", "invalidRequestWarning", "inviteCreate", "inviteDelete", "message", "messageCreate", "messageDelete", "messageDeleteBulk", "messageReactionAdd", "messageReactionRemove", "messageReactionRemoveAll", "messageReactionRemoveEmoji", "messageUpdate", "presenceUpdate", "rateLimit", "ready", "roleCreate", "roleDelete", "roleUpdate", "shardDisconnect", "shardError", "shardReady", "shardReconnecting", "shardResume", "stageInstanceCreate", "stageInstanceDelete", "stageInstanceUpdate", "stickerCreate", "stickerDelete", "stickerUpdate", "threadCreate", "threadDelete", "threadListSync", "threadMembersUpdate", "threadMemberUpdate", "threadUpdate", "typingStart", "userUpdate", "voiceStateUpdate", "warn", "webhookUpdate"]
+	for (let i = 0; i < manifest.eventHandlers.length; i++) {
+		if(!validEvents.includes(manifest.eventHandlers[i])) bootDMs.push("invalid event handler in module:" + moduleName)
+		if(!validEvents.includes(manifest.eventHandlers[i])) return false
+	}
 	return true //shut up shut up shut up shut up shut up i did not forget i didnt i didnt dfjaoiwjefoiajwpfwwwwwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfwfw
 }
 
@@ -117,8 +121,23 @@ function loadModule(moduleName, isCore) {
 		let command
 		if (!isCore) command = require(`${__dirname}/modules/${moduleName}/${commandPath}`); // yay?
 		if (isCore) command = require(`${__dirname}/core/${commandPath}`);
+		let validEvents = ["apiRequest", "apiResponse", "applicationCommandCreate", "applicationCommandDelete", "applicationCommandUpdate", "channelCreate", "channelDelete", "channelPinsUpdate", "channelUpdate", "debug", "emojiCreate", "emojiDelete", "emojiUpdate", "error", "guildBanAdd", "guildBanRemove", "guildCreate", "guildDelete", "guildIntegrationsUpdate", "guildMemberAdd", "guildMemberAvailable", "guildMemberRemove", "guildMembersChunk", "guildMemberUpdate", "guildScheduledEventCreate", "guildScheduledEventDelete", "guildScheduledEventUpdate", "guildScheduledEventUserAdd", "guildScheduledEventUserRemove", "guildUnavailable", "guildUpdate", "interaction", "interactionCreate", "invalidated", "invalidRequestWarning", "inviteCreate", "inviteDelete", "message", "messageCreate", "messageDelete", "messageDeleteBulk", "messageReactionAdd", "messageReactionRemove", "messageReactionRemoveAll", "messageReactionRemoveEmoji", "messageUpdate", "presenceUpdate", "rateLimit", "ready", "roleCreate", "roleDelete", "roleUpdate", "shardDisconnect", "shardError", "shardReady", "shardReconnecting", "shardResume", "stageInstanceCreate", "stageInstanceDelete", "stageInstanceUpdate", "stickerCreate", "stickerDelete", "stickerUpdate", "threadCreate", "threadDelete", "threadListSync", "threadMembersUpdate", "threadMemberUpdate", "threadUpdate", "typingStart", "userUpdate", "voiceStateUpdate", "warn", "webhookUpdate"]
+		if(command.registerEventHandlers) {
+			let addedHandlers = []
+			command.registerEventHandlers(function (eventName, handler) {
+				if(!validEvents.includes(eventName)) bootDMs.push("invalid event handler in module:" + moduleName)
+				if(!validEvents.includes(eventName)) return
+				if(addedHandlers.includes(eventName)) bootDMs.push("duplicate event handler in module:" + moduleName)
+				if(addedHandlers.includes(eventName)) return
+				if(!moduleManifest.eventHandlers.includes(eventName)) bootDMs.push("undeclared event handler in module:" + moduleName)
+				if(!moduleManifest.eventHandlers.includes(eventName)) return
+				addedHandlers.push(eventName)
+				client.on(eventName, handler)
+			})
+		}
 		client.commands.set(commandName, command);
 	});
+
 }
 
 function uninstallModule(moduleName) {
@@ -166,7 +185,7 @@ client.on('messageCreate', message => {
 	}
 
 	if(message.content == "please give me the commands") {
-		message.reply("ok here:" + client.commands.toJSON());
+		message.reply("ok here:" + JSON.stringify(client.commands.toJSON()));
 	} //adding new commands t;he old school way
 
 	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
