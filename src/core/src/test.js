ee5ed63d5a427cc9cd595bfeb61f88d527dfb280
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-05-13 11:27:23 +0200

    Delete test.js

--- src/core/src/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    execute: async function(message, args, apis) {
-        let sharkdb = apis.apis["shark-db-db"].api;
-        sharkdb.getUser(message.author.id, data => {
-            message.channel.send(JSON.stringify(data));
-        })
-    }
-}
