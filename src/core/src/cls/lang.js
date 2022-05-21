f20d4d6ba3e05879e137cf3281d14d7f871ee401
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-05-21 11:50:42 +0300

    Update lang.js

--- src/core/src/cls/lang.js
+++ src/core/src/cls/lang.js
@@ -3,8 +3,8 @@ module.exports = {
         const config = require("../../../../config.json");
         let cls = util.apis["core-cls"].api;
         if(!config.owners.includes(message.author.id)) return message.reply(cls.getString("core", "error.permission"));
-        if (util.apis["core-cls"].api.setLang(args[0])) {
-            message.channel.send({content: `${util.apis["core-cls"].api.getString("core", "lang.success").replace("{0}", args[0])}`, allowedMentions: { parse: [] }});
+        if (cls.setLang(args[0])) {
+            message.channel.send({content: `${cls.getString("core", "lang.success").replace("{0}", args[0])}`, allowedMentions: { parse: [] }});
         }
     }
 }
